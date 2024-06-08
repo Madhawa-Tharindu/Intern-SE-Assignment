d1a7b8d3774a8136d974af35798d91845831a901
Madhawa-Tharindu <madhawath@gmail.com>
2024-06-08 16:04:58 +0530

    created the static side bar

---
+++ app/components/sideBar/sideBar.tsx
@@ -0,0 +1,22 @@
+'use client';
+import './sideBar.css';
+import Link from "next/link";
+import SidebarPill from "./sideBarPill";
+import { SIDEBAR_TITLE } from '@/app/constants/constants';
+
+const Sidebar: React.FC = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-header">
+                <p className="h3 h-min content-center justify-center items-center">{SIDEBAR_TITLE}</p>
+            </div>
+            <div className="my-16">
+                <Link href="/">
+                    <SidebarPill />
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,44 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        'h3': '24px',
+        'h4': '20px',
+        'large': '16px',
+        'normal': '14px',
+        'small': '12px',
+      },
+      fontWeight: {
+        'semi-bold': '600',
+        'medium': '500',
+        'regular': '400',
+      },
+      colors: {
+        'magenta': 'rgba(188, 0, 109, 1)',
+        'stroke': 'rgba(208, 213, 221, 1)',
+        'grey': 'rgba(117, 117, 117, 1)',
+        'green': 'rgba(33, 150, 83, 1)',
+        'yellow': 'rgba(242, 201, 76, 1)',
+      },
+      backgroundColor: {
+        'purple': 'rgba(51, 7, 79, 1)',
+        'light-purple': 'rgba(235, 230, 237, 0.1)',
+        'yellow': 'rgba(242, 201, 76, 1)',
+        'light-yellow': 'rgba(242, 201, 76, 0.1)',
+        'blue': 'rgba(47, 128, 237, 1)',
+        'red': 'rgba(235, 87, 87, 1)',
+        'light-green': 'rgba(232, 245, 233, 1)',
+        'grey': 'rgba(117, 117, 117, 1)'
+      },
+      screens: {
+        'xs': '320px',
+      }
     },
   },
   plugins: [],
-};
+}
 export default config;
