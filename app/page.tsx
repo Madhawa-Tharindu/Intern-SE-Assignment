e5ebbaad75aa786673463a1e7f241b4e4f93a2cf
Madhawa-Tharindu <madhawath@gmail.com>
2024-06-08 21:14:26 +0530

    created the Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Sidebar from "./components/sideBar/sideBar";
+import Header from "./components/header/header";
 
 const Dashboard: React.FC = () => {
   return (
     <main className="font-inter">
       <div className="flex xs:flex-col sm:flex-col md:flex-row lg:flex-row">
         <Sidebar />
+        <div className="w-full h-screen flex flex-col">
+        <Header />
+        </div>
       </div>
     </main>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,10 @@ const config: Config = {
       },
       screens: {
         'xs': '320px',
+        'sm': '640px', //test
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
       }
     },
   },
